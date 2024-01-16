d89f385eb5b5115ac006964476f5bb5394124156
Michael Zhao <michael.yj.zhao@gmail.com>
2024-01-16 13:59:28 +0900

    Updated build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        externalDir: true | {
+            enabled: true,
+            silent: true,
+       },
+    }
+}
 
 module.exports = nextConfig
